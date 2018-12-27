13bf840486a4bc489991cd19b218fa80ada36814
Michał Sałaban <michal@salaban.info>
2018-12-27 23:14:33 +0000

    Move script to examples

--- examples/runpool.py
+++ examples/runpool.py
@@ -9,19 +9,11 @@ from monerowalletpool import WalletsManager, WalletPool
 _log = logging.getLogger(__name__)
 
 class DirPool(WalletPool):
+    """A pool instance watching over a directory of wallets.
+    Each wallet file is supposed to be named by its master address.
+    """
     def __init__(self, manager, **kwargs):
-        addresses = collections.deque()
-        for i in os.listdir(manager.directory):
-            if not i.endswith('.keys'):
-                continue
-            try:
-                addr = monero.address.Address(i.replace('.keys', ''))
-            except ValueError:
-                pass
-            if os.path.exists(os.path.join(manager.directory, str(addr))):
-                addresses.append(addr)
-            else:
-                addresses.appendleft(addr)
+        addresses = manager.list_wallets()
         self._addresses = itertools.cycle(addresses)
         _log.info('Pool has {} addresses.'.format(len(addresses)))
         super(DirPool, self).__init__(manager, **kwargs)
@@ -29,14 +21,20 @@ class DirPool(WalletPool):
     def next_addr(self):
         return next(self._addresses)
 
+    def wallet_started(self, ctrl):
+        _log.info('Started: {}'.format(ctrl.address[:6]))
+
     def wallet_synced(self, ctrl):
-        print(ctrl.incoming)
-        print(ctrl.outgoing)
+        _log.info('{} Incoming: [{}]'.format(ctrl.address[:6], ','.join(ctrl.wallet.incoming())))
+        _log.info('{} Outgoing: [{}]'.format(ctrl.address[:6], ','.join(ctrl.wallet.outgoing())))
         ctrl.shut_down = True
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
+    if len(sys.argv) != 2:
+        print('Usage: {} <directory>'.format(*sys.argv), file=sys.stderr)
+        sys.exit(1)
     pool = DirPool(
             WalletsManager(directory=sys.argv[1], net='stagenet', daemon_port=38081),
             daemon_port=38081)
-    pool.run()
+    pool.main_loop()
